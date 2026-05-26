3ff0449297bd9832b3e28d27ad11054ead715c1f
riveryunny <gayunphone@gmail.com>
2026-05-27 02:53:14 +0900

    feat: 공용 컴포넌트 구현

---
+++ src/components/common/ConfirmModal.tsx
@@ -0,0 +1,47 @@
+interface ConfirmModalProps {
+  open: boolean;
+  message: string;
+  onCancel: () => void;
+  onConfirm: () => void;
+  cancelText?: string;
+  confirmText?: string;
+}
+
+export const ConfirmModal = ({
+  open,
+  message,
+  onCancel,
+  onConfirm,
+  cancelText = "취소",
+  confirmText = "확인",
+}: ConfirmModalProps) => {
+  if (!open) return null;
+
+  return (
+  <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40">
+    <div className="w-[300px] rounded-2xl bg-white px-6 py-8 shadow-xl">
+        <div className="flex flex-col items-center gap-7 mt-2">
+            <p className="text-center text-base font-semibold text-black whitespace-pre-line">
+            {message}
+            </p>
+
+            <div className="flex gap-3">
+            <button
+                type="button"
+                className="h-10 w-[100px] rounded-xl bg-field text-base font-normal text-black"
+                onClick={onCancel}>
+                {cancelText}
+            </button>
+
+            <button
+                type="button"
+                className="h-10 w-[100px] rounded-xl bg-black text-base font-normal text-white"
+                onClick={onConfirm}>
+                {confirmText}
+            </button>
+            </div>
+        </div>
+        </div>
+  </div>
+);
+};
--- src/pages/FriendsPage.tsx
+++ src/pages/FriendsPage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const FriendsPage = () => {
-  return <div>홈 상단 알림 부분 - 친구 페이지</div>;
+  return <div>홈 상단 알림 부분 - 친구 추가</div>;
 };
 
 export default FriendsPage;
