d3b27a799c4eb38e6692998ac10a0b0e10222ecb
riveryunny <gayunphone@gmail.com>
2026-06-05 17:47:22 +0900

    feat: 워터마크 배치 수정

--- src/components/ocr/OcrWaterMark.tsx
+++ src/components/ocr/OcrWaterMark.tsx
@@ -9,7 +9,7 @@ export default function OcrWatermark({ userCode }: OcrWatermarkProps) {
 
   return (
     <div className="pointer-events-none fixed inset-0 z-0 overflow-hidden">
-      <div className="-mx-10 -my-20 grid h-[130vh] w-[130vw] rotate-[-18deg] grid-cols-2 gap-y-10 opacity-[0.08]">
+      <div className="mx-3 my-10 grid h-[130vh] w-[130vw] rotate-[-18deg] grid-cols-2 gap-y-10 opacity-[0.09]">
         {Array.from({ length: 12 }).map((_, index) => (
           <div
             key={index}
